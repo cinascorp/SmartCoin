8d0ddd7cbc592139f5730ee746b45aab3023cba2
sina naqshbandi <47489547+cinascorp@users.noreply.github.com>
2023-08-23 18:33:13 +0330

    Update SmartCoin.sol

--- SmartCoin.sol
+++ SmartCoin.sol
@@ -1,25 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity ^0.8.18;
 
 contract SmartCoin {
-    IERC20 public token;
+    // ERC20-like Token Implementation
+    string public constant name = "Smart Coin";
+    string public constant symbol = "SMC";
+    uint256 public constant decimals = 18;
+    uint256 public totalSupply = 80000000;
+    mapping(address => uint256) public balanceOf;
+
+    // Article Management
+    struct Article {
+        string content;
+        bool isVerified;
+    }
+    mapping(address => Article[]) public userArticles;
     mapping(bytes32 => bool) public articleHashes;
+    uint256 public constant REWARD_AMOUNT = 1;
 
-    constructor(address _tokenAddress) {
-        token = IERC20(_tokenAddress);
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event ArticleSubmitted(address indexed user, string article);
+    event ArticleVerified(address indexed user, uint256 articleIndex);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = initialSupply;
     }
 
-    function storeArticleHash(bytes32 _hash) public {
-        require(!articleHashes[_hash], "Article already submitted");
-        articleHashes[_hash] = true;
+    // Token Transfer Function
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
 
-    function distributeReward(address _recipient, uint256 _amount) public {
-        require(token.balanceOf(address(this)) >= _amount, "Insufficient balance");
-        token.transfer(_recipient, _amount);
+    // Article Submission Function
+    function submitArticle(string memory content) public {
+        Article memory newArticle = Article(content, false);
+        userArticles[msg.sender].push(newArticle);
+        emit ArticleSubmitted(msg.sender, content);
     }
 
-    // Additional functions as needed
+    // Article Verification and Reward Function
+    function verifyArticle(address user, uint256 articleIndex, bytes32 articleHash) public {
+        require(articleIndex < userArticles[user].length, "Invalid article index");
+        require(!userArticles[user][articleIndex].isVerified, "Article already verified");
+        require(!articleHashes[articleHash], "Article hash already submitted");
+
+        userArticles[user][articleIndex].isVerified = true;
+        articleHashes[articleHash] = true;
+
+        balanceOf[user] += REWARD_AMOUNT;
+        totalSupply += REWARD_AMOUNT;
+
+        emit ArticleVerified(user, articleIndex);
+    }
 }
+
