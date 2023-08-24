ae9a8845d1bf720742b64cd53282c88d969a5364
sina naqshbandi <47489547+cinascorp@users.noreply.github.com>
2023-08-24 04:06:30 +0330

    Update openai.js

--- openai.js
+++ openai.js
@@ -1,57 +1,52 @@
-// Function to send a request to OpenAI's text.davinci-003 model
-function sendRequestToOpenAI(articleContent, apiKey) {
-  // Endpoint for the text.davinci-003 model
-  const endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";
+// Function to send the article to OpenAI
+function sendToOpenAI(articleContent) {
+  const apiKey = document.getElementById('apiKey').value;
+  const prompt = "SmartCoin: " + articleContent;
 
-  // Prompt to be sent to OpenAI (customize as needed)
-  const prompt = `SmartCoin Article Verification: ${articleContent}`;
-
-  // Request payload
-  const payload = {
-    prompt: prompt,
-    max_tokens: 150
-  };
-
-  // Request headers
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${apiKey}`
-  };
-
-  // Send the request using the Fetch API
-  fetch(endpoint, {
+  fetch("https://api.openai.com/v1/engines/text-davinci-003/completions", {
     method: "POST",
-    headers: headers,
-    body: JSON.stringify(payload)
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 100
+    })
   })
   .then(response => response.json())
-  .then(data => processResponse(data))
-  .catch(error => console.error("Error sending request to OpenAI:", error));
+  .then(data => {
+    document.getElementById('openaiResponse').innerText = data.choices[0].text;
+    // Additional logic to handle the response, such as rewarding the user
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
 }
 
-// Function to process the response from OpenAI
-function processResponse(data) {
-  // Extract the response text
-  const responseText = data.choices[0].text;
+// Function to submit the article
+function submitArticle() {
+  const fileInput = document.getElementById('fileInput');
+  const articleText = document.getElementById('articleText');
+  let articleContent;
 
-  // Process the response (customize as needed)
-  // For example, you can verify the article, update the UI, etc.
-
-  // Example: Display the response in the UI
-  document.getElementById('openaiResponse').innerText = responseText;
+  if (fileInput.files.length > 0) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      articleContent = e.target.result;
+      sendToOpenAI(articleContent);
+    };
+    reader.readAsText(fileInput.files[0]);
+  } else {
+    articleContent = articleText.value;
+    sendToOpenAI(articleContent);
+  }
 }
 
 // Function to hide the API key input field
 function hideApiKey() {
-  document.getElementById('apiKeyMenu').style.display = "none";
+  document.getElementById('apiKeyMenu').style.display = 'none';
 }
 
-// Example: Add an event listener to the "Submit Article" button
-document.getElementById('submitArticle').addEventListener('click', () => {
-  const articleContent = document.getElementById('article').value;
-  const apiKey = document.getElementById('apiKey').value;
-  sendRequestToOpenAI(articleContent, apiKey);
-});
-
-// Example: Add an event listener to the "Hide" button for the API key
-document.getElementById('hideApiKey').addEventListener('click', hideApiKey);
+// Add event listener to the submit button
+document.getElementById('submitButton').addEventListener('click', submitArticle);
